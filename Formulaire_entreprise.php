e4a02819c088e497fd11d07df6bdd1888c30e5a8
montbuleaugentelettitouan <titouan.gentelet@gmail.com>
2023-11-23 17:32:49 +0100

    v23/11/2023

--- Formulaire_entreprise.php
+++ Formulaire_entreprise.php
@@ -20,35 +20,28 @@ include('fonctionality/bdd.php');
 <div id="layoutSidenav_content"> <!-- body de la page -->
     <main>
         <div class="container-fluid px-4"> <!-- div de page -->
-            <h1 class="mt-4">Ajout d'un stage </h1>
+            <h1 class="mt-4">Proposition de stage </h1>
             <br>
 
             <!----------------------------Section entreprise------------------------------------------->
-
             <div class="card mb-4">      <!--div de section 1 -->
                 <div class="card-header">
-                    <!--div de encadré 1 -->
-                    <form method="post" enctype="multipart/form-data">
-                        <!-- Other form elements -->
-
-                        <div class="form-group">
-                            <i class="far fa-file-pdf"></i>
-                            <label for="pdfFile">Déposer un fichier PDF :</label>
-                            <input type="file" class="form-control-file" id="pdfFile" name="pdfFile" accept=".pdf">
-                        </div>
-                    </form>
-                </div><!--fin div de encadré 1 -->
-
+                    <center>
+                    <b>Merci de renseigner dans le formulaire ci-après les informations essentielles concernant le stage que vous proposez.
+                    <br>
+                    Merci de déposer un PDF de cette proposition de stage qui sera mis à disposition des étudiants.</b>
+                    </center>
+                </div>
                 <br>
 
-                <p>* : Saisie obligatoire</p>
+                <p><b><span style="color: red;">*</span></b> : Saisie obligatoire</p>
 
                 <!-- Tableau ajout du stage -->
 
                 <form method ="post">
                     <div class="card-body"><!--div de tableau 1 -->
                         <div class="form-group">
-                            <label><h5> Nom de l'entreprise* :</h5></label>
+                            <label><h5> Nom de l'entreprise<b><span style="color: red;">*</span></b> :</h5></label>
                             <input type="text" class="form-control" id="Entreprise" name="Entreprise" required>
                         </div>
                         <br>
@@ -58,56 +51,73 @@ include('fonctionality/bdd.php');
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Sujet du stage* :</h5></label>
-                            <input type="text" class="form-control" id="Sujet" name="Sujet" required>
+                            <label><h5> Intitulé du poste<b><span style="color: red;">*</span></b> :</h5></label>
+                            <input type="text" class="form-control" id="titre" name="titre" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Ville* :</h5></label>
+                            <label><h5> Ville<b><span style="color: red;">*</span></b> :</h5></label>
                             <input type="text" class="form-control" id="Ville" name="Ville" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Pays* :</h5></label>
+                            <label><h5> Pays<b><span style="color: red;">*</span></b> :</h5></label>
                             <input type="text" class="form-control" id="Pays" name="Pays" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Missions du stage* :</h5></label>
+                            <label><h5> Missions du stage<b><span style="color: red;">*</span></b> :</h5></label>
                             <input type="text" class="form-control" id="Missions" name="Missions" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Compétences attendues* :</h5></label>
+                            <label><h5> Compétences attendues<b><span style="color: red;">*</span></b> :</h5></label>
                             <input type="text" class="form-control" id="Competences" name="Competences" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Nombre de stages proposés* :</h5></label>
-                            <input type="number" class="form-control" id="Nbstage" name="Nbstage" required>
+                            <label><h5> Nombre de stages proposés<b><span style="color: red;">*</span></b> :</h5></label>
+                            <input type="number" class="form-control" id="nbPoste" name="nbPoste" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Mail(s) de la (des) personne(s) à contacter*:</h5></label>
-                            <input type="text" class="form-control" id="MailContact" name="MailContact" required>
+                            <label><h5> Mail de la personne à contacter<b><span style="color: red;">*</span></b>:</h5></label>
+                            <input type="text" class="form-control" id="mailContact" name="mailContact" required>
                         </div>
                         <br>
                         <div class="form-group">
-                            <label><h5> Représentant(s) :</h5></label>
+                            <label><h5> Si connu, représentant au forum stage:</h5></label>
                             <input type="text" class="form-control" id="Representant" name="Representant">
                         </div>
                         <br>
-                        <input type="submit" class="btn btn-warning" name="ValidAjoutEntreprise" value="Ajouter">
+                        <div class="card mb-4">      <!--div de section 1 -->
+                            <div class="card-header">
+
+                                <form method="post" enctype="multipart/form-data">
+
+
+                                    <div class="form-group">
+                                        <i class="far fa-file-pdf"></i>
+                                        <label for="pdfFile">Déposer un fichier PDF<b><span style="color: red;">*</span></b> :</label>
+                                        <input type="file" class="form-control-file" id="pdfFile" name="pdfFile" accept=".pdf" required>
+                                    </div>
+                                </form>
+                            </div>
+                            <br>
+                        <input type="submit" class="btn btn-warning" name="ValidAjoutStage" value="Ajouter">
                         <br>
                     </div><!--fin div de tableau 1 -->
                 </form>
                 <?php
-                if (isset($_POST['ValidAjoutEntreprise'])) {
+                if (isset($_POST['ValidAjoutStage'])) {
                     $nomEntreprise = $_POST['Entreprise'];
                     $nomEntreprise= strtoupper($nomEntreprise);
 
-                    $Site = $_POST['ChoiceSite'];
-                    $Site= strtoupper($Site);
+                    $nomSite = $_POST['ChoiceSite'];
+                    $nomSite= strtoupper($nomSite);
+
+                    $titre = $_POST['titre'];
+                    $titre= ucfirst($titre);
 
                     $ville = $_POST['Ville'];
                     $ville = ucfirst($ville);
@@ -115,62 +125,67 @@ include('fonctionality/bdd.php');
                     $pays = $_POST['Pays'];
                     $pays= strtoupper($pays);
 
-                    $contactRH = $_POST['contactRH'];
+                    $description = $_POST['Missions'];
+                    $description = ucfirst($description);
+
+                    $competences = $_POST['Competences'];
+                    $competences = ucfirst($competences);
+
+                    $nbPoste = $_POST['nbPoste'];
+
+                    $mailContact = $_POST['mailContact'];
+
+                    $representant = $_POST['Representant'];
+                    $representant= strtoupper($representant);
+
+                    $pdf = $_FILES['pdfFile'];
 
                     //test des variables
                     /*echo $nomEntreprise;
-                    echo $Site;
+                    echo $nomSite;
                     echo $ville;
                     echo $pays;
-                    echo $contactRH*/
+                    echo $description;
+                    echo $competences;
+                    echo $nbPoste;
+                    echo $mailContact;
+                    echo $Representant;*/
 
-                    //on vérifie si l'entreprise existe déja
-                    $verifE = $bdd ->prepare('SELECT * from entreprise WHERE nomEntreprise = ?');
-                    $verifE->execute(array($nomEntreprise));
-                    $resultatENT = $verifE ->fetch();
-                    $countENT = $verifE->rowcount();
+                    //On rajoute le stage dans la BDD
 
-                    if ( $countENT != 0) {
-                        //si oui, on récupère l'id
-                        $recupE = $bdd->prepare('SELECT * FROM entreprise WHERE nomEntreprise = ?');
-                        $recupE->execute(array($nomEntreprise));
-                        $resultRecup = $recupE->fetch();
+                    $reqinsertS = $bdd -> prepare ('INSERT INTO offre (titre, description, competences, nbPoste, mailContact, representant) values (?,?,?,?,?,?) ');
+                    $reqinsertS -> execute (array($titre, $description, $competences, $nbPoste, $mailContact, $representant));
+                    $resultatS = $reqinsertS -> fetch();
 
-                        $idEnt = !empty($resultRecup['idEntreprise']) ? $resultRecup['idEntreprise'] : NULL ;
+                    //On récupère son id
 
-                    } else {   //s'il y a rien alors on rajoute dans la bdd et on récupère l'id
+                    $recupE = $bdd->prepare('SELECT * FROM offre WHERE description = ?');
+                    $recupE->execute(array($description));
+                    $resultRecup = $recupE->fetch();
 
-                        $reqinsertE = $bdd -> prepare ('INSERT INTO entreprise (nomEntreprise) VALUES (?)');
-                        $reqinsertE -> execute (array($nomEntreprise));
-                        $resultatE = $reqinsertE-> fetch();
+                    $idOffre = !empty($resultRecup['idOffre']) ? $resultRecup['idOffre'] : NULL ;
 
-                        //on récupère l'identifiant de l'entreprise qui vient d'être ajouté
+                    /* A REFAIRE
 
-                        $recupE = $bdd->prepare('SELECT * FROM entreprise WHERE nomEntreprise = ?');
-                        $recupE->execute(array($nomEntreprise));
-                        $resultRecup = $recupE->fetch();
+                    //on rajoute dans la bdd et on récupère l'id
 
-                        $idEnt = !empty($resultRecup['idEntreprise']) ? $resultRecup['idEntreprise'] : NULL ;
-                    }
+                    $reqinsertE = $bdd -> prepare ('INSERT INTO entreprise (nomEntreprise) VALUES (?)');
+                    $reqinsertE -> execute (array($nomEntreprise));
+                    $resultatE = $reqinsertE-> fetch();
+
+                    //on récupère l'identifiant du stage qui vient d'être ajouté
+
+                    $recupE = $bdd->prepare('SELECT * FROM entreprise WHERE nomEntreprise = ?');
+                    $recupE->execute(array($nomEntreprise));
+                    $resultRecup = $recupE->fetch();
+
+                    $idEnt = !empty($resultRecup['idEntreprise']) ? $resultRecup['idEntreprise'] : NULL ;
                     //on vérifie si le site existe déjà
                     $verifS = $bdd ->prepare('SELECT * from site WHERE nomSite =? and ville = ? and idEntreprise = ? ');
                     $verifS->execute(array($Site,$ville,$idEnt));
                     $resultatSite = $verifS ->fetch();
                     $countSite = $verifS->rowcount();
-
-                    if($countSite !=0) {
-                        echo "Ce site existe déjà";
-
-                    } else{
-
-                        //si non, on l'ajoute
-                        $reqinsertS = $bdd -> prepare ('INSERT INTO site (nomSite, ville, pays, contactRH, idEntreprise) values (?,?,?,?,?) ');
-                        $reqinsertS -> execute (array($Site, $ville, $pays, $contactRH, $idEnt));
-                        $resultatS = $reqinsertS -> fetch();
-
-                        echo "Le site a été ajoutée avec succès";
-
-                    }
+                    */
                 }
                 ?>
             </div><!--fin div de section 1 -->
